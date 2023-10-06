ef3d79de693c80e9324094e5cd99c620934bed8b
Joao Henrique <joaoadsistemas@gmail.com>
2023-10-06 13:12:11 -0300

    Exception handling - fingById

--- src/main/java/com/joaosilveira/course/services/UserService.java
+++ src/main/java/com/joaosilveira/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.joaosilveira.course.services;
 
 import com.joaosilveira.course.entities.User;
 import com.joaosilveira.course.repositories.UserRepository;
+import com.joaosilveira.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/joaosilveira/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.joaosilveira.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+
+}
